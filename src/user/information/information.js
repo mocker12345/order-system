629363cd3272e9e4b4fcbe29604f808a4f520b06
rancongjie <95633676@qq.com>
2015-12-18 16:56:04 +0800

    js

--- src/user/information/information.js
+++ src/user/information/information.js
@@ -1,3 +1,37 @@
-app.controller('infoController', ['$scope', function ($scope) {
-
+app.controller('infoController', ['$scope', '$rootScope', '$location', 'api', function ($scope, $location, $rootScope, api) {
+  if ($rootScope.nowId) {
+    api.user.load($rootScope.nowId).get().then(function (res) {
+      if (res.success) {
+        $scope.data = {
+          username: res.data.username,
+          password: res.data.password,
+          mobile: res.data.mobile,
+          address: res.data.addresses.location
+        };
+      } else {
+        alert(res.errorMsg);
+      }
+    }, function (err) {
+      console.error(err);
+    });
+  } else {
+    alert('请先登录');
+    $location.path('/login');
+  }
+  $scope.update = function () {
+    $scope.newData = {
+      password: $scope.data.password,
+      mobile: $scope.data.mobile,
+      address: $scope.data.address
+    };
+    api.user.update($rootScope.nowId).get().then(function (res) {
+      if (res.success) {
+        alert('保存成功');
+      } else {
+        alert(res.errorMsg);
+      }
+    }, function (err) {
+      console.error(err);
+    });
+  }
 }]);
